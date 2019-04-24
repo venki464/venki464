5b37aa5c365205e030af6370714a1ae1af4998ce
Venkateswara Rao Meda <venkateswararao.meda2@t-mobile.com>
2019-04-24 07:59:55 +0530

    What is Viper

--- ViperSample/ViewController.swift
+++ ViperSample/ViewController.swift
@@ -6,6 +6,17 @@
 //  Copyright © 2019 Venkateswara Rao Meda. All rights reserved.
 //
 
+/*Here is the sample demo for VIPER - View, Interactor, Presenter, Entity and Router
+ 
+ View - In view section, we only write the code for app interface related stuff.
+ Interactor - It has the bussiness logic, where we have make the API calls or any data related stuff even might be caluclations, depending on how the data required for view.
+ Presenter - It is the mediator for bothe View and Interactor. It takes the input from the view and ask Interactor to provide the required data and passes to View.
+ Entity - Contains plain model classes / structs.
+ Router - Does the wire-framming. From the inputs of Interactor it shows the screen which to present.
+ 
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
